7aae7e8fd5568014b6cb9e57d745ba2872f86910
ardianq <ardian.qerimi3@gmail.com>
2020-01-10 17:44:23 +0100

    Add models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nothyP.apps.NothypConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
